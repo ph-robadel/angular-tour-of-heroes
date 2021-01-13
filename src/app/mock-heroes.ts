a2dc6cae0a24a8511f0bfa414af89300189a48f9
Pedro Henrique Robadel <pedro.camara@itixti.com.br>
2021-01-13 10:01:03 -0300

    Ending session 2. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Pedro' },
+    {id: 12, name: 'Henrique' },
+    {id: 13, name: 'Robadel' },
+    {id: 14, name: 'Marco' },
+    {id: 15, name: 'Douglas' },
+    {id: 16, name: 'Gabriel' },
+    {id: 17, name: 'João' },
+    {id: 18, name: 'Luan' },
+    {id: 19, name: 'Igor' },
+    {id: 20, name: 'Marlan' },
+]
